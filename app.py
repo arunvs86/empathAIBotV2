3e1b025e7c660c7391dbce051e72fb38458def6b
Arun Soundararajan <arunsoundararajan@Aruns-MacBook-Air.local>
2025-08-04 19:50:44 +0100

    Fix port for Azure (8000)

--- app.py
+++ app.py
@@ -197,4 +197,4 @@ def test_cors():
     return jsonify({"message": "CORS is working!"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), debug=True)
